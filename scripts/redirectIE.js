33699d65aa2b88e918e2f0908dc352c04af2d573
Aiden <33640860+SomeAspy@users.noreply.github.com>
2022-01-01 18:44:57 -0500

    drop IE support

---
+++ scripts/redirectIE.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 Aiden Baker
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+if(window.document.documentMode){window.location.replace("https://www.aspy.dev/internal/IE.html")}
