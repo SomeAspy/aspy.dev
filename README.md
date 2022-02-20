fa48dca9f57e51fca51fc801252e1bdaf2d17af8
Aiden <amb@aspy.dev>
2022-02-20 17:32:28 -0500

    minor and major changes

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# aspy.dev
-My Personal website.
-<br>
-I work on this in my free time.
-<br>
+# Aspy.dev
+My Personal website that I work on in my free time.
+
 It may not look the best but I'm proud of it. I'm not with HTML or CSS, but its been a lot of fun and a great learning experience working on this. When I started this project I had no idea how CSS worked.
-<br>
+
 This site also incorporates some stuff for its subdomains, like the stuff under /external
+
+I develop this website with Chrome Canary in mind, and also test for Firefox Nightly with lower priority. This is simply due to the market share for the browsers. Testing with Edge will generally not be done.
+That being said, if you find any issues please open an issue!
+## Hosting
+I host the base domain on Cloudflare Pages. Subdomains on the site are generally hosted using Vercel, or more rarely, GitHub Pages.
+## Goals
+Some goals are listed in the TODO.txt file.
+However, the main goals I have for this site are as follows:
+### Modern Standards
+One of my goals for the site is to continually use the latest features, for example, WebP, and AVIF or JPEG XL when support is finalized. This website entirely does not support Internet Explorer, for these reasons.
+### Technical Perfectionism
+I aim for a 100 in all categories of Google's Lighthouse auditing system, as well as having no errors or warnings in the console.
+### Unique
+I do not use templates or color pallettes. All CSS and HTML is made by me, by hand. I also do not use React or alternatives to React. (Mainly as I don't fully understand it.)
--- projects/HS/project4/project4.js
+++ projects/HS/project4/project4.js
@@ -2,22 +2,20 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+// Edited from original for use in the site. Original: https://github.com/SomeAspy/SchoolRepos/tree/main/HighSchool/JavaScript/project4
 let fails = 6 //set a counter for the amount of chances
-// eslint-disable-next-line no-unused-vars
 function takeGuess(){ //this is called by the button
     return(prompt("Enter a letter")) //prompt user to enter a letter, and return the output
-}
+};
 function refresh() { //refreshes the display
     document.getElementById("word").innerHTML = display; //tells HTML to update the page to match the current progress
 }
-// eslint-disable-next-line no-undef
-let chosen = nouns[Math.floor(Math.random()*nouns.length)] //pick a random word from words.js
+const chosen = nouns[Math.floor(Math.random()*nouns.length)] //pick a random word from words.js
 console.log(chosen) //log that word to console, for easy debugging or cheating
 console.log("?".repeat(chosen.length)) //log the empty guess things, more debugging
 console.log(chosen.length) //log the length of the chosen word, for debugging
 let display = "?".repeat(chosen.length) //set the display variable to the amount of spaces we need
 refresh() //refresh the shown display
-// eslint-disable-next-line no-unused-vars
 function process() { //called by the button to process guesses
 if (chosen.includes(window.guess)) { //check if the chosen word includes the guessed letter
     display = display.split("") //turn the progress display into an array
@@ -26,7 +24,6 @@ if (chosen.includes(window.guess)) { //check if the chosen word includes the gue
     console.log(display) //log the display for debugging
     refresh() //refresh the display
 } else { //if it the letter is not in the word, execute below
-    // eslint-disable-next-line no-undef
     $(document.getElementById(fails)).removeClass() //remove circles
     fails -=1 // subtract 1 from fails
     if(fails==0) { //if fails = 0
