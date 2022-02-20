0cd3eaa38c348001d56d54b60bfc45f9836f3804
Aiden <amb@aspy.dev>
2022-02-20 18:07:59 -0500

    Module Test

--- projects/HS/project4/project4.js
+++ projects/HS/project4/project4.js
@@ -3,33 +3,34 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 // Edited from original for use in the site. Original: https://github.com/SomeAspy/SchoolRepos/tree/main/HighSchool/JavaScript/project4
-let fails = 6 //set a counter for the amount of chances
-function takeGuess(){ //this is called by the button
-    return(prompt("Enter a letter")) //prompt user to enter a letter, and return the output
+import{noun}from'./words.json';
+let fails=6;
+function takeGuess(){
+    return(prompt("Enter a letter"));
 };
-function refresh() { //refreshes the display
-    document.getElementById("word").innerHTML = display; //tells HTML to update the page to match the current progress
+function refresh(){
+    document.getElementById("word").innerHTML=display;
 }
-const chosen = nouns[Math.floor(Math.random()*nouns.length)] //pick a random word from words.js
-console.log(chosen) //log that word to console, for easy debugging or cheating
-console.log("?".repeat(chosen.length)) //log the empty guess things, more debugging
-console.log(chosen.length) //log the length of the chosen word, for debugging
-let display = "?".repeat(chosen.length) //set the display variable to the amount of spaces we need
-refresh() //refresh the shown display
-function process() { //called by the button to process guesses
-if (chosen.includes(window.guess)) { //check if the chosen word includes the guessed letter
-    display = display.split("") //turn the progress display into an array
-    display.splice(chosen.indexOf(window.guess), 1, window.guess) //find and replace blanks with the guessed letter (only one at a time, cuz I'm lazy)
-    display = display.join("") //turn the array back into a string
-    console.log(display) //log the display for debugging
-    refresh() //refresh the display
-} else { //if it the letter is not in the word, execute below
-    $(document.getElementById(fails)).removeClass() //remove circles
-    fails -=1 // subtract 1 from fails
-    if(fails==0) { //if fails = 0
-        display = chosen //show the word
-        refresh() //refresh display
-        document.getElementById("button").remove() //remove button
+const chosen=noun[Math.floor(Math.random()*noun.length)]
+console.log(chosen)
+console.log("?".repeat(chosen.length))
+console.log(chosen.length)
+let display="?".repeat(chosen.length)
+refresh()
+function process(){
+if(chosen.includes(window.guess)){
+    display=display.split("")
+    display.splice(chosen.indexOf(window.guess),1,window.guess)
+    display=display.join("")
+    console.log(display)
+    refresh()
+}else{
+    $(document.getElementById(fails)).removeClass()
+    fails-=1
+    if(fails==0){
+        display=chosen
+        refresh()
+        document.getElementById("button").remove()
     }
 }
 }
