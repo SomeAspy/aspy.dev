694a34b517b655e0d88390bf61ed7afcf6aa013d
Aiden <amb@aspy.dev>
2022-02-28 13:16:23 -0500

    404 and contact

---
+++ internal/404/404.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Aiden Baker
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+function redirect() {
+    window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+}
