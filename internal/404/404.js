4a3eba8731c58d2bd34c6fad7f1e5fbd355fe1d2
Aiden <amb@aspy.dev>
2022-03-11 11:37:33 -0500

    fuck

--- internal/404/404.js
+++ internal/404/404.js
@@ -2,6 +2,6 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-document.getElementsByClassName('troll').addEventListener('click', function () {
+document.getElementById('troll').addEventListener('click', function () {
     window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
 });
