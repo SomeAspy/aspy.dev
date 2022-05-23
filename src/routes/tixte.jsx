30381603a880290c2784ce94e4bedb5daefa629d
Aiden <amb@aspy.dev>
2022-05-23 12:59:42 -0400

    Remove Solid Meta

--- src/routes/tixte.jsx
+++ src/routes/tixte.jsx
@@ -3,25 +3,28 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { MetaProvider, Title, Meta } from 'solid-meta';
-
 import NavBar from '../components/navbar/navbar.jsx';
 import Footer from '../components/footer/footer.jsx';
 import '../CSS/home.css';
 
+/*
+We want these headers but fuck me I guess
+    <meta charset='utf-8' />
+    <meta
+    name='viewport' content='width=device-width, initial-scale=1.0'/>
+    <link rel='shortcut-icon' content='./src/images/tixte.svg' />
+    <title>Aiden's CDN</title>
+    <meta name='og:description' content="Aiden's CDN is powered by Tixte! A free file hosting service."/>
+    <meta name='og:title' content="Aiden's CDN" />
+    <meta property='og:image' content='./src/images/tixte.gif' />
+    <meta name='theme-color' content='#26a9e1' />
+    <meta rel='shortcut-icon' content='./src/images/tixte.svg' />
+    <link rel='stylesheet' href='../CSS/home.css' />
+*/
+
 export default function Tixte() {
     return (
-        <MetaProvider>
-            <Title>Aiden's CDN</Title>
-            <Meta name='og:title' content="Aiden's CDN" />
-            <Meta
-                name='og:description'
-                content="Aiden's CDN is powered by Tixte! A free file hosting service."
-            />
-            <Meta name='og:image' content='./src/images/tixte.gif' />
-            <Meta name='og:color' content='#26a9e1' />
-            <Meta rel='shortcut-icon' content='./src/images/tixte.svg' />
-
+        <>
             <div class='background' />
             <NavBar />
             <div class='centerText center'>
@@ -31,6 +34,6 @@ export default function Tixte() {
                 </h3>
             </div>
             <Footer github='tixte.jsx' />
-        </MetaProvider>
+        </>
     );
 }
