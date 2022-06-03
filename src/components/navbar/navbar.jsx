516c0d76a4e17721cab79309aa44228a32708640
Aiden <amb@aspy.dev>
2022-06-03 15:22:19 -0400

    combine contact, home, and about into one page change font to Manrope Update packages Fix mobile appearance of contact buttons

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,9 +9,7 @@ export default function NavBar() {
     return (
         <div className='navBar'>
             <a href='https://aspy.dev'>Home</a>
-            <a href='https://aspy.dev/about'>About Me</a>
             <a href='https://aspy.dev/projects'>Projects</a>
-            <a href='https://aspy.dev/contact'>Contact</a>
             <a href='https://blog.aspy.dev'>Blog</a>
         </div>
     );
--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -13,16 +13,7 @@ export default function About() {
         <>
             <div class='background' />
             <NavBar />
-            <div className='textBlock'>
-                <h1 className='header'>Hi!</h1>
-                <p>
-                    My name is Aiden, and I absolutely love fixing things and
-                    technology, especially computers. I've worked several IT
-                    jobs and spend a lot of time helping people with computers.
-                    I am studying Computer Science and hope to work in the IT
-                    field.
-                </p>
-            </div>
+
             <div className='discord'></div>
             <Footer github='about.jsx' />
         </>
