e859dee415e8af7978559d888d48b160992ae12b
Aiden <amb@aspy.dev>
2022-06-03 15:28:07 -0400

    update router to reflect changes

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,7 @@ import { lazy } from 'solid-js';
 import { Routes, Route, Router } from 'solid-app-router';
 
 const Home = lazy(() => import('./routes/home.jsx'));
-const About = lazy(() => import('./routes/about.jsx'));
 const Projects = lazy(() => import('./routes/projects.jsx'));
-const Contact = lazy(() => import('./routes/contact.jsx'));
 const Tixte = lazy(() => import('./routes/tixte.jsx'));
 const NotFound = lazy(() => import('./routes/404.jsx'));
 
@@ -15,8 +13,6 @@ render(
             <Routes>
                 <Route path='/' component={Home} />
                 <Route path='/projects' component={Projects} />
-                <Route path='/contact' component={Contact} />
-                <Route path='/about' component={About} />
                 <Route path='/tixte' component={Tixte} />
                 <Route path='*' component={NotFound} />
             </Routes>
