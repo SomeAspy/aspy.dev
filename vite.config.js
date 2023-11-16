c859acd6e57559a64f5f86271d026251beec6adc
Aiden <amb@aspy.dev>
2023-11-16 16:09:44 -0500

    Package and config Updates

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,10 @@ export default defineConfig({
         target: 'esnext',
         polyfillDynamicImport: false,
     },
+    server: {
+        port: 3000
+    },
+    preview: {
+        port: 3000
+    }
 });
